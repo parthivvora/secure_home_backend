d6b50155b62bcf76993be6fc2efaf92a331ad857
parthivvora <v.parthiv004@gmail.com>
2025-04-21 12:12:36 +0530

    chore: initialize project with package.json and vercel.json configuration
    - Added package.json with dependencies for express, cors, firebase-admin, and nodemon.
    - Created vercel.json for deployment configuration on Vercel.

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const admin = require("firebase-admin");
+const cors = require("cors");
+
+const app = express();
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+app.get("/", (req, res) => {
+  res.send("FCM Notification Server is running");
+});
+
+app.post("/send-notification", async (req, res) => {
+  const { token, title, body } = req.body;
+  console.log("🚀 ~ app.post ~ req.body:", req.body);
+
+  if (!token || !title || !body) {
+    return res
+      .status(400)
+      .json({ error: "token, title, and body are required" });
+  }
+
+  const message = {
+    notification: {
+      title,
+      body,
+    },
+    token,
+  };
+
+  try {
+    const response = await admin.messaging().send(message);
+    console.log("✅ Notification sent:", response);
+    return res.status(200).json({ success: true, messageId: response });
+  } catch (error) {
+    console.error("❌ Error sending FCM:", error);
+    return res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server started at http://localhost:${PORT}`);
+});
